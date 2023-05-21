e08911deca2bc67c7d4ca9aec0eef415d6eb555c
macosta2015 <macosta2015@my.fit.edu>
2023-05-20 23:40:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@
 // 7. Drawing utilities DONE
 // 8. Draw functions DONE
 
+
+
+
 import React, { useRef } from "react";
-// import logo from './logo.svg';
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
